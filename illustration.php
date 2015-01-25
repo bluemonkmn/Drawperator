4b09f743bddd897f46ea36bc081ee0d05a6a457b
Benjamin Marty <bluemonkmn@gmail.com>
2015-01-25 13:16:49 -0600

    Finish bare-bones implementation

--- illustration.php
+++ illustration.php
@@ -17,7 +17,7 @@ case 'GET':
    if ($cmd == 'illustration') {
       $sql = 'SELECT format,illustration FROM tg_illustration';
    } else {
-      $sql = 'SELECT tg_illustration.id,format,fk_phrase,clientaddress,name FROM tg_illustration JOIN tg_user ON fk_user = tg_user.id';
+      $sql = 'SELECT tg_illustration.id,format,fk_phrase predecessor,clientaddress,name user FROM tg_illustration JOIN tg_user ON fk_user = tg_user.id';
       if (isset($id))
          $cmd = 'single';
    }
@@ -25,52 +25,52 @@ case 'GET':
    if (isset($id))
       $sql .= " WHERE tg_illustration.id=?";      
 
-   if ($cmd == 'list')
-      echo '<illustrations>';
-
-   if (!($stmt = $conn->prepare($sql))) {
+   if (!($stmt = $conn->prepare($sql)))
       die ($conn->error);
-   }
    if (isset($id))
       $stmt->bind_param('i', $id);
    $stmt->execute();
    if ($cmd == 'illustration') {
       $stmt->bind_result($format,$illustration);
-      if (fetch()) {
+      if ($stmt->fetch()) {
          switch($format) {
-            case 'PNG':
-               header('Content-Type: image/png');
-               break;
             case 'GIF':
                header('Content-Type: image/gif');
                break;
             case 'SVG':
                header('Content-Type: image/svg+xml');
                break;
+            default:
+               header('Content-Type: image/png');
+               break;
          }
          print $illustration;
       }
    } else {
-      $stmt->bind_result($id,$format,$fk_phrase,$clientaddress,$illustrationuser);
-      while ($stmt->fetch()) {
-         ?><illustration id="<?= $id ?>" format="<?= $format ?>" predecessor="<?= $fk_phrase ?>" clientaddress="<?= !empty($clientaddress)?inet_ntop($clientaddress):'' ?>" user="<?= $illustrationuser ?>" />
-<?php
+      header('Content-Type: application/json');
+      $rowset = $stmt->get_result();
+      $results = array();
+      while ($result = $rowset->fetch_array(MYSQL_ASSOC)) {
+         $result['clientaddress'] = $result['clientaddress'] ? inet_ntop($result['clientaddress']) : '';
+         array_push($results, $result);
       }
    }
    $stmt->close();
    if ($cmd == 'list')
-      echo "</illustrations>";
+      echo json_encode($results);
+   elseif ($cmd == 'single')
+      echo json_encode($results[0]);
    break;
 case 'POST':
    if (empty($username))
       die("User name required.");
-   if (!($stmt = $conn->prepare('SELECT id FROM tg_user WHERE name=?'));
+   if (!($stmt = $conn->prepare('SELECT id FROM tg_user WHERE name=?')))
       die ($conn->error);
    $stmt->bind_param('s', $username);
    $stmt->execute();
    $stmt->bind_result($uid);   
    if (!$stmt->fetch()) {
-      if (!($stmt2 = $conn->prepare('INSERT INTO tg_user(name) VALUES(?)')));
+      if (!($stmt2 = $conn->prepare('INSERT INTO tg_user(name) VALUES(?)')))
          die ($conn->error);      
       $stmt2->bind_param('s', $username);
       $stmt2->execute();
@@ -81,15 +81,22 @@ case 'POST':
    if (empty($uid))
       die("Failed to get user id.");
    
-   if (!($stmt = $conn->prepare('INSERT INTO tg_illustration(id,fk_phrase,illustration,clientaddress,fk_user) VALUES(?,?,BINARY ?,BINARY ?,?)'));
+   if (!($illustration = base64_decode($_REQUEST['illustration'])))
+      die("Failed to decode picture data");
+   
+   if (!($stmt = $conn->prepare('INSERT INTO tg_illustration(id,fk_phrase,format,illustration,clientaddress,fk_user) VALUES(?,?,?,BINARY ?,BINARY ?,?)')))
       die ($conn->error);
-   $illustrationid = mt_rand() . mt_rand(0,999999999);
-   $clientip = inet_pton($_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']));
-   $stmt->bind_param('iissi', $illustrationid, $_REQUEST["predecessor"], $_REQUEST["illustration"], $clientip, $uid);
-   $stmt->execute();
+   $result = array('id' => mt_rand() . mt_rand(0,999999999),
+      'format' => $_REQUEST['format'],
+      'predecessor' => $_REQUEST['predecessor'],
+      'clientaddress' => $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']),
+      'user' => $username);
+   $stmt->bind_param('iisssi', $result['id'], $result['predecessor'], $result['format'], $illustration, inet_pton($result['clientaddress']), $uid);
+   if (!$stmt->execute())
+      die($conn->error);
    $stmt->close();
-   ?><illustration id="<?= $illustrationid ?>" predecessor="<?= $_REQUEST["predecessor"] ?>" clientaddress="<?= !empty($clientip)?inet_ntop($clientip):'' ?>" user="<?= $username ?>" />
-<?php   
+   header('Content-Type: application/json');
+   echo json_encode($result);
    break;
 }
 $conn->close();
--- phrase.php
+++ phrase.php
@@ -1,8 +1,6 @@
 <?php
 include_once "connect.php";
-header('Content-Type: text/xml');
-echo '<?xml version="1.0" encoding="UTF-8" ?>';
-echo "\n";
+header('Content-Type: application/json');
 
 $conn = new mysqli($servername, $dbuser, $password, $dbname);
 // Check connection
@@ -15,38 +13,40 @@ case 'GET':
    if (!empty($_REQUEST['request']) && preg_match('/^\d+$/', $_REQUEST['request'], $matches)) {
       $id = $matches[0];
    }
-   $sql = 'SELECT tg_phrase.id,fk_illustration,phrase,clientaddress,name FROM tg_phrase JOIN tg_user ON fk_user = tg_user.id';
+   $sql = 'SELECT tg_phrase.id,fk_illustration predecessor,phrase,clientaddress,name user FROM tg_phrase JOIN tg_user ON fk_user = tg_user.id';
    $single = false;
    if (isset($id)) {
       $single = true;
       $sql .= " WHERE tg_phrase.id=?";      
-   } else
-      echo "<phrases>";
-   if (!($stmt = $conn->prepare($sql))) {
-      die ($conn->error);
    }
+   if (!($stmt = $conn->prepare($sql)))
+      die ($conn->error);
    if ($single)
       $stmt->bind_param('i', $id);
    $stmt->execute();
    $stmt->bind_result($id,$fk_illustration,$phrase,$clientaddress,$phraseuser);
-   while ($stmt->fetch()) { ?>
-<phrase id="<?= $id ?>" predecessor="<?= $fk_illustration ?>" phrase="<?= htmlspecialchars($phrase)?>" clientaddress="<?= !empty($clientaddress)?inet_ntop($clientaddress):'' ?>" user="<?= $phraseuser ?>" />
-<?php
+   $rowset = $stmt->get_result();
+   $results = array();
+   while ($result = $rowset->fetch_array(MYSQL_ASSOC)) {
+      $result['clientaddress'] = !empty($result['clientaddress']) ? inet_ntop($result['clientaddress']) : '';
+      array_push($results, $result);
    }
    $stmt->close();
-   if (!$single)
-      echo "</phrases>";
+   if ($single)
+      echo json_encode($results[0]);
+   else
+      echo json_encode($results);
    break;
 case 'POST':
    if (empty($username))
       die("User name required.");
-   if (!($stmt = $conn->prepare('SELECT id FROM tg_user WHERE name=?'));
+   if (!($stmt = $conn->prepare('SELECT id FROM tg_user WHERE name=?')))
       die ($conn->error);   
    $stmt->bind_param('s', $username);
    $stmt->execute();
    $stmt->bind_result($uid);   
    if (!$stmt->fetch()) {
-      if (!($stmt2 = $conn->prepare('INSERT INTO tg_user(name) VALUES(?)')));
+      if (!($stmt2 = $conn->prepare('INSERT INTO tg_user(name) VALUES(?)')))
          die ($conn->error);
       $stmt2->bind_param('s', $username);
       $stmt2->execute();
@@ -57,15 +57,17 @@ case 'POST':
    if (empty($uid))
       die("Failed to get user id.");
    
-   if (!($stmt = $conn->prepare('INSERT INTO tg_phrase(id,fk_illustration,phrase,clientaddress,fk_user) VALUES(?,?,?,BINARY ?,?)')));
-      die ($conn->error);   
-   $phraseid = mt_rand() . mt_rand(0,999999999);
-   $clientip = inet_pton($_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']));
-   $stmt->bind_param('iissi', $phraseid, $_REQUEST["predecessor"], $_REQUEST["phrase"], $clientip, $uid);
+   if (!($stmt = $conn->prepare('INSERT INTO tg_phrase(id,fk_illustration,phrase,clientaddress,fk_user) VALUES(?,?,?,BINARY ?,?)')))
+      die ($conn->error);
+   $result = array('id' => mt_rand() . mt_rand(0,999999999),
+      'predecessor' => $_REQUEST['predecessor'],
+      'phrase' => $_REQUEST['phrase'],
+      'clientaddress' => $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']),
+      'user' => $username);
+   $stmt->bind_param('iissi', $result['id'], $result['predecessor'], $result['phrase'], inet_pton($result['clientaddress']), $uid);
    $stmt->execute();
-   $stmt->close();?>
-<phrase id="<?= $phraseid ?>" predecessor="<?= $_REQUEST["predecessor"] ?>" phrase="<?= htmlspecialchars($_REQUEST["phrase"])?>" clientaddress="<?= !empty($clientip)?inet_ntop($clientip):'' ?>" user="<?= $username ?>" />
-<?php   
+   $stmt->close();
+   echo json_encode($result);
    break;
 }
 $conn->close();
